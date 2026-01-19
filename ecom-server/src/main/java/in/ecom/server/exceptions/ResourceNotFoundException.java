16d3993b603c8888d2c95e76618678269cad4c16
Nkbalaji2002 <nithishkumarbsc@gmail.com>
2026-01-19 21:11:17 +0530

    modify files
    modified resource exception and category service implementation

--- ecom-server/src/main/java/in/ecom/server/exceptions/ResourceNotFoundException.java
+++ ecom-server/src/main/java/in/ecom/server/exceptions/ResourceNotFoundException.java
@@ -7,14 +7,14 @@ public class ResourceNotFoundException extends RuntimeException{
     String fieldName;
     Long fieldId;
 
-    public ResourceNotFoundException(String message, String resourceName, String field, String fieldName) {
+    public ResourceNotFoundException(String resourceName, String field, String fieldName) {
         super(String.format("%s not found with %s : %s", resourceName, field, fieldName));
         this.resourceName = resourceName;
         this.field = field;
         this.fieldName = fieldName;
     }
 
-    public ResourceNotFoundException(String message, String resourceName, String field, Long fieldId) {
+    public ResourceNotFoundException(String resourceName, String field, Long fieldId) {
         super(String.format("%s not found with %s : %s", resourceName, field, fieldId));
         this.resourceName = resourceName;
         this.field = field;
--- ecom-server/src/main/java/in/ecom/server/service/impl/CategoryServiceImpl.java
+++ ecom-server/src/main/java/in/ecom/server/service/impl/CategoryServiceImpl.java
@@ -1,12 +1,11 @@
 package in.ecom.server.service.impl;
 
+import in.ecom.server.exceptions.ResourceNotFoundException;
 import in.ecom.server.model.Category;
 import in.ecom.server.repository.CategoryRepository;
 import in.ecom.server.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -29,7 +28,7 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public String delete(Long categoryId) {
         Category category = categoryRepository.findById(categoryId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
+                .orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", categoryId));
 
         categoryRepository.delete(category);
         return "Category with categoryId : " + categoryId + " deleted successfully!!!";
@@ -38,7 +37,7 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public Category update(Category category, Long categoryId) {
         Category updatedCategory = categoryRepository.findById(categoryId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
+                .orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", categoryId));
 
         updatedCategory.setCategoryName(category.getCategoryName());
 
