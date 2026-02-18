c6ffc67712a8c33dc7af9c9673a90d958aa4567f
Nkbalaji2002 <nithishkumarbsc@gmail.com>
2026-02-18 06:29:26 +0530

    products - dtos

---
+++ ecom-server/src/main/java/in/ecom/server/payload/ProductDTO.java
@@ -0,0 +1,20 @@
+package in.ecom.server.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO {
+
+    private Long productId;
+    private String productName;
+    private String image;
+    private Integer quantity;
+    private double price;
+    private double discount;
+    private double specialPrice;
+
+}
