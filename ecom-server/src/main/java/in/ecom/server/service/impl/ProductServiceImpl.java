af224dcd9ef41f8a4b84fa2bd17d9482470f03b1
Nkbalaji2002 <nithishkumarbsc@gmail.com>
2026-02-22 09:29:55 +0530

    making use of FileService and application.properties

--- ecom-server/src/main/java/in/ecom/server/service/impl/ProductServiceImpl.java
+++ ecom-server/src/main/java/in/ecom/server/service/impl/ProductServiceImpl.java
@@ -7,18 +7,16 @@ import in.ecom.server.payload.ProductDTO;
 import in.ecom.server.payload.ProductResponse;
 import in.ecom.server.repository.CategoryRepository;
 import in.ecom.server.repository.ProductRepository;
+import in.ecom.server.service.FileService;
 import in.ecom.server.service.ProductService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.UUID;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -31,6 +29,12 @@ public class ProductServiceImpl implements ProductService {
 
     @Autowired
     private ModelMapper modelMapper;
+
+    @Autowired
+    private FileService fileService;
+
+    @Value("${project.image}")
+    private String path;
 
     @Override
     public ProductDTO addProduct(Long categoryId, ProductDTO productDTO) {
@@ -116,8 +120,7 @@ public class ProductServiceImpl implements ProductService {
 
         /* upload image to server */
         /* get the file name of uploaded image */
-        String path = "images/";
-        String fileName = uploadImage(path, image);
+        String fileName = fileService.uploadImage(path, image);
 
         /* updating the new file name to the product */
         productFromDb.setImage(fileName);
@@ -127,29 +130,5 @@ public class ProductServiceImpl implements ProductService {
 
         /* return DTO after mapping product to DTO */
         return modelMapper.map(updatedProduct, ProductDTO.class);
-    }
-
-    private String uploadImage(String path, MultipartFile file) throws IOException {
-        /* file names of current or original file */
-        String originalFileName = file.getOriginalFilename();
-
-        /* Generate a unique file name */
-        String randomId = UUID.randomUUID().toString();
-
-        /* eg:- mat.jpg --> 1234 ---> 1234.jpg */
-        String fileName = randomId.concat(originalFileName.substring(originalFileName.lastIndexOf('.')));
-        String filePath = path + File.separator + fileName;
-
-        /* Check if path exist and create */
-        File folder = new File(path);
-        if (!folder.exists()) {
-            folder.mkdir();
-        }
-
-        /* upload to server */
-        Files.copy(file.getInputStream(), Paths.get(filePath));
-
-        /* returning file name */
-        return fileName;
     }
 }
