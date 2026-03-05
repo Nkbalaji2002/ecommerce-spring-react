e3efdb32846683587695d0d9d26943c4dac8bb3c
Nkbalaji2002 <nithishkumarbsc@gmail.com>
2026-03-05 06:11:38 +0530

    custom userRepository and userDetailsServiceImpl

---
+++ ecom-server/src/main/java/in/ecom/server/security/services/repos/UserRepository.java
@@ -0,0 +1,12 @@
+package in.ecom.server.security.services.repos;
+
+import in.ecom.server.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserName(String username);
+}
