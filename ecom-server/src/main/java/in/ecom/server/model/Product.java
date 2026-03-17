c6cb845aa3f120f4aece4a52c5ef8ad97368fcba
Nkbalaji2002 <nithishkumarbsc@gmail.com>
2026-03-17 20:55:25 +0530

    relationship with cart

--- ecom-server/src/main/java/in/ecom/server/model/Product.java
+++ ecom-server/src/main/java/in/ecom/server/model/Product.java
@@ -5,6 +5,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity(name = "products")
 @Getter
 @Setter
@@ -43,5 +46,8 @@ public class Product {
     @ManyToOne
     @JoinColumn(name = "seller_id")
     private User user;
+
+    @OneToMany(mappedBy = "product", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
+    private List<CartItem> products = new ArrayList<>();
 
 }
