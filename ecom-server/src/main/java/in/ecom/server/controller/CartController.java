8a0e6a8e05c423194960acbd34957ea7c212b410
Nkbalaji2002 <nithishkumarbsc@gmail.com>
2026-03-22 21:46:02 +0530

    fetch user carts

--- ecom-server/src/main/java/in/ecom/server/controller/CartController.java
+++ ecom-server/src/main/java/in/ecom/server/controller/CartController.java
@@ -1,7 +1,10 @@
 package in.ecom.server.controller;
 
+import in.ecom.server.model.Cart;
 import in.ecom.server.payload.CartDTO;
+import in.ecom.server.repository.CartRepository;
 import in.ecom.server.service.CartService;
+import in.ecom.server.util.AuthUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +17,17 @@ import java.util.List;
 public class CartController {
 
     @Autowired
+    private CartRepository cartRepository;
+
+    @Autowired
+    private AuthUtil authUtil;
+
+    @Autowired
     private CartService cartService;
 
     @PostMapping("/products/{productId}/quantity/{quantity}")
     public ResponseEntity<?> addProductToCart(@PathVariable Long productId,
-                                                    @PathVariable Integer quantity) {
+                                              @PathVariable Integer quantity) {
         CartDTO cartDTO = cartService.addProductToCart(productId, quantity);
         return new ResponseEntity<>(cartDTO, HttpStatus.CREATED);
     }
@@ -27,6 +36,15 @@ public class CartController {
     public ResponseEntity<?> getCarts() {
         List<CartDTO> cartDTOS = cartService.getAllCarts();
         return new ResponseEntity<>(cartDTOS, HttpStatus.FOUND);
+    }
+
+    @GetMapping("/users/cart")
+    public ResponseEntity<?> getCartById() {
+        String emailId = authUtil.loggedInEmail();
+        Cart cart = cartRepository.findCartByEmail(emailId);
+        Long cartId = cart.getCartId();
+        CartDTO cartDTO = cartService.getCart(emailId, cartId);
+        return new ResponseEntity<>(cartDTO, HttpStatus.OK);
     }
 
 }
--- ecom-server/src/main/java/in/ecom/server/service/impl/CartServiceImpl.java
+++ ecom-server/src/main/java/in/ecom/server/service/impl/CartServiceImpl.java
@@ -18,7 +18,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 public class CartServiceImpl implements CartService {
@@ -109,16 +108,32 @@ public class CartServiceImpl implements CartService {
 
         List<CartDTO> cartDTOS = carts.stream()
                 .map(cart -> {
-                   CartDTO cartDTO = modelMapper.map(cart, CartDTO.class);
-                   List<ProductDTO> productDTOS = cart.getCartItems().stream()
-                           .map(p -> modelMapper.map(p.getProduct(), ProductDTO.class))
-                           .toList();
+                    CartDTO cartDTO = modelMapper.map(cart, CartDTO.class);
+                    List<ProductDTO> productDTOS = cart.getCartItems().stream()
+                            .map(p -> modelMapper.map(p.getProduct(), ProductDTO.class))
+                            .toList();
 
-                   cartDTO.setProducts(productDTOS);
-                   return cartDTO;
+                    cartDTO.setProducts(productDTOS);
+                    return cartDTO;
                 }).toList();
 
         return cartDTOS;
+    }
+
+    @Override
+    public CartDTO getCart(String emailId, Long cartId) {
+        Cart cart = cartRepository.findCartByEmailAndCartId(emailId, cartId);
+        if (cart == null) {
+            throw new ResourceNotFoundException("Cart", "cartId", cartId);
+        }
+        CartDTO cartDTO = modelMapper.map(cart, CartDTO.class);
+        cart.getCartItems().forEach(c -> c.getProduct().setQuantity(c.getQuantity()));
+        List<ProductDTO> products = cart.getCartItems().stream()
+                .map(p -> modelMapper.map(p.getProduct(), ProductDTO.class))
+                .collect(Collectors.toList());
+
+        cartDTO.setProducts(products);
+        return cartDTO;
     }
 
     private Cart createCart() {
