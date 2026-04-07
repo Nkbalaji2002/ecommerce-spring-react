595fb9d28998ed96c27b09df2df549f86d517641
Nkbalaji2002 <nithishkumarbsc@gmail.com>
2026-04-07 06:15:36 +0530

    create order and order-Item entity

---
+++ ecom-server/src/main/java/in/ecom/server/model/Order.java
@@ -0,0 +1,45 @@
+package in.ecom.server.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+    @Email
+    @Column(nullable = false)
+    private String email;
+
+    @OneToMany(mappedBy = "order", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private LocalDate localDate;
+
+//    @OneToOne
+//    @JoinColumn(name = "payment_id")
+//    private Payment payment;
+
+    private Double totalAmount;
+
+    private String orderStatus;
+
+    @ManyToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+}
---
+++ ecom-server/src/main/java/in/ecom/server/model/OrderItem.java
@@ -0,0 +1,33 @@
+package in.ecom.server.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "order_items")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderItemId;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private Integer quantity;
+
+    private double discount;
+
+    private double orderedProductPrice;
+
+}
