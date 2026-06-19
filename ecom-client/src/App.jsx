957950c2c767b2eb404bd9f18b6408a8e37f16c2
Nkbalaji2002 <nithishkumarbsc@gmail.com>
2026-06-19 06:27:13 +0530

    File Structures

--- ecom-client/src/App.jsx
+++ ecom-client/src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { FaBeer } from "react-icons/fa";
-import Products from "./components/Products";
 import { Provider } from "react-redux";
 import store from "./store/reducers/store";
 import { BrowserRouter } from "react-router-dom";
+import Products from "./components/products/Products";
 
 const App = () => {
   return (
