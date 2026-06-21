e73bd4a33a8da90077e158166ac5b1650468d25a
Nkbalaji2002 <nithishkumarbsc@gmail.com>
2026-06-21 22:33:13 +0530

    setting and building Cart functionality

--- ecom-client/src/App.jsx
+++ ecom-client/src/App.jsx
@@ -8,6 +8,8 @@ import Home from "./components/home/Home";
 import Navbar from "./components/shared/Navbar";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import { Toaster } from "react-hot-toast";
+import Cart from "./components/cart/Cart";
 
 const App = () => {
   return (
@@ -20,8 +22,12 @@ const App = () => {
             <Route path="/products" Component={Products} />
             <Route path="/about" Component={About} />
             <Route path="/contact" Component={Contact} />
+            <Route path="/cart" Component={Cart} />
           </Routes>
         </Router>
+        <>
+          <Toaster position="bottom-center" />
+        </>
       </Provider>
     </>
   );
--- ecom-client/src/store/actions/index.js
+++ ecom-client/src/store/actions/index.js
@@ -61,3 +61,34 @@ export const fetchCategories = (queryString) => async (dispatch) => {
     });
   }
 };
+
+export const addToCart =
+  (data, qty = 1, toast) =>
+  (dispatch, getState) => {
+    // Find the Product
+    const { products } = getState().products;
+
+    const getProduct = products.find(
+      (item) => item.productId === data.productId
+    );
+
+    // Check for Stocks
+    const isQuantityExist = getProduct.quantity >= qty;
+
+    // If in Stocks -> add
+    if (isQuantityExist) {
+      dispatch({
+        type: "ADD_CART",
+        payload: {
+          ...data,
+          quantity: qty,
+        },
+      });
+
+      toast.success(`${data?.productName} added to the Cart`);
+      localStorage.setItem("cartItems", JSON.stringify(getState().carts.cart));
+    } else {
+      // If not -> error
+      toast.error(`Out of Stock`);
+    }
+  };
